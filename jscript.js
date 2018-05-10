19cc3dc08e53fa916c304b8259f05f0181a0371b
Annie Kao <anniekao@trentu.ca>
2018-05-10 22:33:35 +0200

    added addGreen(), addWhite() and eventListener to change colour when hovering over a div

--- jscript.js
+++ jscript.js
@@ -1,13 +1,32 @@
-function createDiv (size) {
+function createDiv(size) {
   for (i=0; i <= size - 1; i++){
     let newDiv = document.createElement("div");
-    let gridDiv = document.querySelector(".grid");
-    gridDiv.appendChild(newDiv);
+    let grid = document.querySelector(".grid");
+
+    newDiv.className = "gridDiv";
+    grid.appendChild(newDiv);
   };
+}
+
+function addGreen(){
+  this.style.backgroundColor = "#00ff92";
+}
+
+function addWhite(){
+  this.style.backgroundColor = "white";
 }
 
 let size = 16;
 createDiv(size);
+
+let gridDiv = document.getElementsByClassName("gridDiv");
+
+for (let i=0; i < gridDiv.length; i++) {
+  gridDiv[i].addEventListener("mouseenter", addGreen, false);
+  gridDiv[i].addEventListener("mouseleave", addWhite, false);
+}
+
+
 
 /*let btnQuery = document.querySelector("#size-input");
 
