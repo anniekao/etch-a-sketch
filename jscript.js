d51f6a74c11e2651289ac5a279b65ac7947db76d
Annie Kao <anniekao@trentu.ca>
2018-05-10 21:12:01 +0200

    added button and js to create a 16x16 grid

---
+++ jscript.js
@@ -0,0 +1,12 @@
+function createDiv (size) {
+  for (i=0; i <= size - 1; i++){
+    let newDiv = document.createElement("div");
+    let gridDiv = document.querySelector(".grid");
+    gridDiv.appendChild(newDiv);
+  };
+}
+
+let size = 16;
+let btnQuery = document.querySelector("#size-input");
+
+btnQuery.addEventListener("click", () => createDiv(size));
